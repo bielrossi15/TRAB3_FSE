c9e7b78f83a99fb3198ca2e6a88abd5a3c9795b6
bielrossi15 <bielrossiborba@gmail.com>
2020-11-29 19:57:26 -0300

    Comitando configuracoes e arquivos sem a logica principal

---
+++ src/led.c
@@ -0,0 +1,13 @@
+#include "led.h"
+#include "driver/gpio.h"
+
+#define LED 2
+
+void init_led() {
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+}
+void set_led_state(int state) {
+    gpio_set_level(LED, state);
+}
+
---
+++ src/led.h
@@ -0,0 +1,7 @@
+#ifndef LED_H
+#define LED_H
+
+void init_led();
+void set_led_state();
+
+#endif
